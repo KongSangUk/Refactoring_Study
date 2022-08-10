e579d2cc9cfaded75204fcf0146d03330fe3ef8d
Kong Sang Uk <qkqhfp19@naver.com>
2022-08-11 02:28:45 +0900

    Movie 클래스 만듬

---
+++ src/Movie.java
@@ -0,0 +1,16 @@
+//Movìe는 간단한 비디오 데이터 클래스다
+
+public class Movie {
+
+        public static final int CHILDRENS = 2;
+        public static final int REGULAR = 0;
+        public static final int NEW_RELEASE = 1;
+        private String _title;
+        private int _priceCode;
+
+        public Movie(String title, int priceCode) {
+            _title = title;
+            _priceCode = priceCode;
+
+        }
+    }
