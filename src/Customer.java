8dc0e5e9a55dffd4bf1ae87ad3163c1e77da867a
Kong Sang Uk <qkqhfp19@naver.com>
2022-08-11 21:43:24 +0900

    feat :  54P 진행 / 수정

--- src/Customer.java
+++ src/Customer.java
@@ -21,27 +21,32 @@ public class Customer {
 
     //
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = getName() + "고객님의 대여 기록/n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-
-            // 경우에 따른 적립 포인트 지급 함수를 호출
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
-
             //이번에 대여하는 비디오 정보와 대여료 출출
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf
-                    (each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(each.getCharge()) + "\n";
         }
-        result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+
+        result += "누적 대여료: " + String.valueOf(getTotalCharge()) + "\n";
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
         return result;
     }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
 }
 
 
--- src/Movie.java
+++ src/Movie.java
@@ -2,23 +2,47 @@
 
 public class Movie {
 
-        public static final int CHILDRENS = 2;
-        public static final int REGULAR = 0;
-        public static final int NEW_RELEASE = 1;
-        private String _title;
-        private int _priceCode;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    private String _title;
+    private int _priceCode;
 
-        public Movie(String title, int priceCode) {
-            _title = title;
-            _priceCode = priceCode;
-        }
-        public int getPriceCode() {
-            return _priceCode;
-        }
-        public void setpriceCode(int arg) {
-            _priceCode = arg;
-        }
-        public String getTitle() {
-            return _title;
-        };
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
     }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+
+    public String getTitle() {
+        return _title;
+    };
+
+//    double getCharge() {
+//        double result = 0;
+//        switch (getMovie().getPriceCode()) {
+//            case Movie.REGULAR:
+//                result += 2;
+//                if (getDaysRented() > 2)
+//                    result += (getDaysRented() - 2) * 1.5;
+//                break;
+//            case Movie.NEW_RELEASE:
+//                result += getDaysRented() * 3;
+//                break;
+//            case Movie.CHILDRENS:
+//                result += 1.5;
+//                if (getDaysRented() > 3)
+//                    result += (getDaysRented() - 3) * 1.5;
+//                break;
+//        }
+//        return result;
+//    }
+
+}
